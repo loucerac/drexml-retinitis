c232bf88abb0d44be14e6f4c43fe74b78b0e98d0
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-01-20 20:07:35 +0100

    Add updating mechanism.

--- run_parser.bash
+++ run_parser.bash
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-UPDATE_TRANSLATION=0
+MYGENE_VERSION=$( date +%Y%m%d )
+GTEX_VERSION="V8"
+DRUGBANK_VERSION="v050108"
+UPDATE=0
 
 THIS_FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DATA_PATH="$THIS_FOLDER/data"
 RAW_FOLDER="${DATA_PATH}/raw"
 INTERIM_FOLDER="${DATA_PATH}/interim"
 FINAL_FOLDER="${DATA_PATH}/final"
-
-GTEX_VERSION="V8"
-DRUGBANK_VERSION="v050108"
-MYGENE_VERSION=$( date +%Y%m%d )
 
 # RAW
 XML_PATH="${RAW_FOLDER}/drugbank-${DRUGBANK_VERSION}.xml.gz"
@@ -27,9 +26,21 @@ GENES_FILTERED_PATH="${FINAL_FOLDER}/genes-drugbank-${DRUGBANK_VERSION}_gtex-${G
 
 PARSER_FOLDER="${THIS_FOLDER}/modules/drugbank-parser"
 
+# Parsing
 python ${PARSER_FOLDER}/parser.py parse $XML_PATH $TSV_PATH
-python ${PARSER_FOLDER}/parser.py translate $TSV_PATH $DB_GENES_PATH --kind drugbank 
-python ${PARSER_FOLDER}/parser.py translate $PARQUET_PATH $GTEX_GENES_PATH --kind gtex 
+
+if test -f "$DB_GENES_PATH"; then
+    echo "$DB_GENES_PATH exists."
+else
+    python ${PARSER_FOLDER}/parser.py translate $TSV_PATH $DB_GENES_PATH --kind drugbank 
+fi
+
+if test -f "$GTEX_GENES_PATH"; then
+    echo "$GTEX_GENES_PATH exists."
+else
+    python ${PARSER_FOLDER}/parser.py translate $PARQUET_PATH $GTEX_GENES_PATH --kind gtex 
+fi
+
 python ${PARSER_FOLDER}/parser.py filter \
     --drugbank-path ${TSV_PATH} \
     --drugbank-genes-path ${DB_GENES_PATH} \
