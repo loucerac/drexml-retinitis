477c981f4ba954d90497b639990b996d4a068fa3
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-02-07 08:49:13 +0100

    Add 02_run_annotator.bash file.

---
+++ 02_run_annotator.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+THIS_FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+R_ENV="${THIS_FOLDER}/.venvs/r"
+R_FNAME="02_HPO_obo_annotations_RP.R"
+R_SRC_PATH="${THIS_FOLDER}/modules/${R_FNAME}"
+conda run -p ${R_ENV} Rscript ${R_SRC_PATH}
