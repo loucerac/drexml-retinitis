1c0d3289d7e359c23aad2681f1567ce747e03d21
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-02-15 11:38:22 +0100

    Fix folder setup.

--- 00_run_hipathia.bash
+++ 00_run_hipathia.bash
@@ -3,5 +3,5 @@
 THIS_FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 R_ENV="${THIS_FOLDER}/.venvs/r"
 R_FNAME="00_GTEx_V8processing.R"
-R_SRC_PATH="${THIS_FOLDER}/modules/${R_FNAME}"
+R_SRC_PATH="${THIS_FOLDER}/modules/r/${R_FNAME}"
 conda run -p ${R_ENV} Rscript ${R_SRC_PATH}
