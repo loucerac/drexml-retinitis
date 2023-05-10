bc0382ad7d8ea39f0346d96ebc44eaa3d41cda8d
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-05-10 17:57:08 +0200

    Move all ml results to ml folder.

--- 04_run_drexml.bash
+++ 04_run_drexml.bash
@@ -7,5 +7,6 @@ conda run -p ${PY_ENV} drexml run --n-gpus 0 --n-cpus -1 ${EXP_PATH}
 ML_FOLDER="${THIS_FOLDER}/results/ml"
 mkdir -p ${ML_FOLDER}
 mv "${THIS_FOLDER}/results/shap*.tsv" ${ML_FOLDER}/.
-conda run -p ${PY_ENV} drexml rename ${THIS_FOLDER}
-conda run -p ${PY_ENV} drexml plot ${THIS_FOLDER}/stability_results_symbol.tsv
+mv "${THIS_FOLDER}/results/stability_results*.tsv" ${ML_FOLDER}/.
+conda run -p ${PY_ENV} drexml rename ${ML_FOLDER}
+conda run -p ${PY_ENV} drexml plot ${ML_FOLDER}/stability_results_symbol.tsv
