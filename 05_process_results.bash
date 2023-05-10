4e362bfef46afade8132b0e32d0a213ad283b01f
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-05-10 18:16:16 +0200

    Run every result processing script.

--- 05_process_results.bash
+++ 05_process_results.bash
@@ -4,12 +4,12 @@ THIS_FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 R_ENV="${THIS_FOLDER}/.venvs/r"
 
 fnames=(
-    #"04_read_filter_shap_drugbank.R"
-    #"05_heatmaps_balloon_tables.R"
-    # "06_RPhallmarks_analysis.R"
-    #"07_Drugs_ATC_statistics.R"
-    # "08_KDT_Kmeans_Clusters.R"
-    # "09_Drugs_SpiderPlot.R"
+    "04_read_filter_shap_drugbank.R"
+    "05_heatmaps_balloon_tables.R"
+    "06_RPhallmarks_analysis.R"
+    "07_Drugs_ATC_statistics.R"
+    "08_KDT_Kmeans_Clusters.R"
+    "09_Drugs_SpiderPlot.R"
     "10_circle_chord_diagram.R"
 )
 
