374ddec8ba8835e5a9d3f2a26757dc74fdc30700
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-05-23 06:28:26 +0200

    Remove obsolete step.

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ conda env update --prune -p ${DREXML_ENV_FOLDER} -f environment_drexml.yml
 R_ENV_FOLDER="./.venvs/r"
 conda env update --prune -p ${R_ENV_FOLDER} -f environment_r.yml
 export PKG_CONFIG_PATH="${R_ENV_FOLDER}/lib/pkgconfig"
-export PKG_LIBS="-liconv"
+#export PKG_LIBS="-liconv"
 conda run -p .venvs/r/ R --vanilla -e 'source("renv/activate.R"); renv::restore()'
