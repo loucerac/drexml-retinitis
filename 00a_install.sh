173efddfd19c9aa1bb81954553c815864865c387
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-05-23 22:25:40 +0200

    Allow GPU installation.

--- 00a_install.sh
+++ 00a_install.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+USE_GPU=$1
 THIS_FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 PARSER_ENV_FOLDER="$THIS_FOLDER/.venvs/drugbank-parser"
 conda env update --prune -p ${PARSER_ENV_FOLDER} -f "$THIS_FOLDER/environment_drugbank-parser.yml"
 
 DREXML_ENV_FOLDER="$THIS_FOLDER/.venvs/drexml"
-conda env update --prune -p ${DREXML_ENV_FOLDER} -f "$THIS_FOLDER/environment_drexml.yml"
+if [[ -z "$USE_GPU" ]]; then
+    env_file="environment_drexml.yml"
+elif [[ "$USE_GPU" -gt 0 ]]; then
+    env_file="environment_drexml_gpu.yml"
+fi
+echo "using ${env_file}"
+
+conda env update --prune -p ${DREXML_ENV_FOLDER} -f "${THIS_FOLDER}/${env_file}"
 
 R_ENV_FOLDER="$THIS_FOLDER/.venvs/r"
 conda env update --prune -p ${R_ENV_FOLDER} -f "$THIS_FOLDER/environment_r.yml"
 export PKG_CONFIG_PATH="${R_ENV_FOLDER}/lib/pkgconfig"
-#export PKG_LIBS="-liconv"
-
 conda run -p $R_ENV_FOLDER R --vanilla -e "source('$THIS_FOLDER/renv/activate.R'); renv::restore()"
 
 # create data folders
