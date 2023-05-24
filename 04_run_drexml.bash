5095cf0dea37875acd5568cc05ff43cd214bfe73
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-05-24 10:54:05 +0200

    Use CUDA only if it has been set by the user.

--- 04_run_drexml.bash
+++ 04_run_drexml.bash
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+USE_GPU=$1
+
+if [[ -z "$USE_GPU" ]]; then
+    n_gpus=0
+elif [[ "$USE_GPU" -gt 0 ]]; then
+    n_gpus=-1
+fi
+
 THIS_FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PY_ENV="${THIS_FOLDER}/.venvs/drexml"
 EXP_PATH="${THIS_FOLDER}/exp_design.env"
-conda run -p ${PY_ENV} drexml run ${EXP_PATH}
+conda run -p ${PY_ENV} drexml run --ngpus $n_gpus --n-cpus -1 ${EXP_PATH}
 ML_FOLDER="${THIS_FOLDER}/results/ml"
 mkdir -p ${ML_FOLDER}
 mv "${THIS_FOLDER}/results/shap"*".tsv" ${ML_FOLDER}/.
