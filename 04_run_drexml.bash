064515d9fda60a8ae4694dfb6e9986d2a4b50dc1
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-05-24 13:56:47 +0200

    Fix bug with argument call.

--- 04_run_drexml.bash
+++ 04_run_drexml.bash
@@ -11,7 +11,7 @@ fi
 THIS_FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PY_ENV="${THIS_FOLDER}/.venvs/drexml"
 EXP_PATH="${THIS_FOLDER}/exp_design.env"
-conda run -p ${PY_ENV} drexml run --ngpus $n_gpus --n-cpus -1 ${EXP_PATH}
+conda run -p ${PY_ENV} drexml run --n-gpus $n_gpus --n-cpus -1 ${EXP_PATH}
 ML_FOLDER="${THIS_FOLDER}/results/ml"
 mkdir -p ${ML_FOLDER}
 mv "${THIS_FOLDER}/results/shap"*".tsv" ${ML_FOLDER}/.
