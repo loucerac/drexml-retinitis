e2dbaf6470bee42205b090e0e198615eff67fd96
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2023-05-27 10:25:24 +0000

    Remove debug test.

--- 00c_run_hipathia.bash
+++ 00c_run_hipathia.bash
@@ -7,5 +7,3 @@ R_ENV="${THIS_FOLDER}/.venvs/r"
 R_FNAME="00_GTEx_V8processing.R"
 R_SRC_PATH="${THIS_FOLDER}/modules/r/${R_FNAME}"
 conda run --no-capture-output --live-stream -p ${R_ENV} Rscript ${R_SRC_PATH}
-echo "diff in tmm"
-diff ./data/final/expreset_Hinorm_gtexV8.rds.feather ~/.data/zenodo/6020480/20230315/expreset_Hinorm_gtexV8.rds.feather
